76ebf503648ebdc9bcd822df651da80abf65c21b
imsorler <imsorler@gmail.com>
2023-02-08 03:24:33 +0300

    adding components & styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from 'react';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={500} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
